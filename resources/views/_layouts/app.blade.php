6a137b57f09e8703015e4ed485f56d9d8971c216
Amrit Twanabasu <amrit.cas@gmail.com>
2016-02-20 19:33:12 +0545

    Added link to Roboto font.

--- resources/views/_layouts/app.blade.php
+++ resources/views/_layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
     <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
+    <link href='https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 </head>
 <body>
