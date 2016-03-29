a4f038739af8dfeff9a5846744b5c8639225d619
Amrit Twanabasu <amrit.cas@gmail.com>
2016-03-29 10:54:33 +0545

    Add Hero Button to Home page.

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -9,6 +9,7 @@
         <div class="hero__bg-container">
             <div class="hero__bg-container-overlay">
                 <h1>Oh! Hi there.<br />In case you're wondering,<br />We provide you with HSEB Notes &amp; Refrences</h1>
+                <a href="{{ route('posts.index') }}" class="hero__btn mdl-button mdl-js-button mdl-button--raised mdl-button--primary mdl-js-ripple-effect">Read what we got</a>
             </div>
         </div>
     </div>
