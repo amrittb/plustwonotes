1e52fd70e2aa5fc78b454dd7deec55ad9e8c620f
Amrit Twanabasu <amrit.cas@gmail.com>
2016-03-29 06:49:41 +0545

    Style post actions.

--- resources/views/_partials/posts/admin/actions.blade.php
+++ resources/views/_partials/posts/admin/actions.blade.php
@@ -1,19 +1,20 @@
 <span class="post-list-item__actions">
     @if($post->isReadable())
-        <a href="{{ $post->read_url }}" target="_blank">
+        <a href="{{ $post->read_url }}" target="_blank" class="post-list-item__action post-list-item-action__read">
             <i class="material-icons">visibility</i>
         </a>
+        {{--<span class="mdl-tooltip" for="post-list-item-action__read">Read Post</span>--}}
     @endif
 
     @if($post->isEditable())
-        <a href="{{ $post->edit_url }}" target="_blank">
+        <a href="{{ $post->edit_url }}" target="_blank" class="post-list-item__action">
             <i class="material-icons">edit</i>
         </a>
     @endif
 
     @if($post->isSoftDeleteable() || $post->isHardDeleteable())
         {!! Form::open(['url' => $post->delete_url,'method' => 'DELETE']) !!}
-            <button type="submit">
+            <button type="submit" class="post-list-item__action">
                 <i class="material-icons">delete</i>
             </button>
         {!! Form::close() !!}
@@ -21,7 +22,7 @@
 
     @if($post->isPublishable())
         {!! Form::open(['url' => $post->publish_url,'method' => 'PATCH']) !!}
-            <button type="submit">
+            <button type="submit" class="post-list-item__action">
                 <i class="material-icons">publish</i>
             </button>
         {!! Form::close() !!}
@@ -29,7 +30,7 @@
 
     @if($post->isDraftable())
         {!! Form::open(['url' => $post->draft_url,'method' => 'PATCH']) !!}
-            <button type="submit">
+            <button type="submit" class="post-list-item__action">
                 <i class="material-icons">archive</i>
             </button>
         {!! Form::close() !!}
