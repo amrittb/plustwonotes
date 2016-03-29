df6f986cf20a383d988319d956d36adfa53bb7cc
Amrit Twanabasu <amrit.cas@gmail.com>
2016-03-29 18:50:40 +0545

    Add favicon to the site.

--- resources/views/_layouts/main.blade.php
+++ resources/views/_layouts/main.blade.php
@@ -4,6 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
+    <link rel="shortcut icon" sizes="16x16 32x32" href="favicon.ico">
+    <link rel="apple-touch-icon" sizes="57x57" href="/img/favicons/favicon-57.png">
+    <link rel="apple-touch-icon-precomposed" sizes="57x57" href="/img/favicons/favicon-57.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/img/favicons/favicon-72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/img/favicons/favicon-114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/img/favicons/favicon-120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/img/favicons/favicon-144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/img/favicons/favicon-152.png">
+    <meta name="application-name" content="Plus Two Notes">
+    <meta name="msapplication-TileImage" content=/img/favicons/favicon-144.png">
+    <meta name="msapplication-TileColor" content="#2A2A2A">
     <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
     <link href='https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
