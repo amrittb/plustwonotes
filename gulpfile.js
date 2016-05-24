73e53d4d6e5789cbbd4ef5dd8a307995ff8cd4bd
Amrit Twanabasu <amrit.cas@gmail.com>
2016-05-24 16:37:59 +0545

    Add Styles to MediumEditor

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ require('laravel-elixir-vueify');
 
 var paths = {
 	mdl : './node_modules/material-design-lite',
-    dialogPolyfill : './node_modules/dialog-polyfill'
+    dialogPolyfill : './node_modules/dialog-polyfill',
+    mediumEditor: './node_modules/medium-editor/src/sass'
 };
 
 elixir(function(mix) {
@@ -28,6 +29,8 @@ function mixScripts(mix){
  */
 function mixCss(mix){
     mix.sass('app.scss',null,{
-        includePaths: [].concat(paths.mdl + '/src').concat(paths.dialogPolyfill)
+        includePaths: [].concat(paths.mdl + '/src')
+                        .concat(paths.dialogPolyfill)
+                        .concat(paths.mediumEditor)
     });
 }
