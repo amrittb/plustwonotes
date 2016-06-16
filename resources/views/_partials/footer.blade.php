2b611b07b6a5fd1aade015f816c59a7fabe983bd
Amrit Twanabasu <amrit.cas@gmail.com>
2016-06-17 05:41:36 +0545

    Add creator's name

--- resources/views/_partials/footer.blade.php
+++ resources/views/_partials/footer.blade.php
@@ -3,6 +3,9 @@
         <h6 class="mdl-typography--text-center">
             Copyright &copy; Plus Two Notes<br/> {{ \Carbon\Carbon::now()->format('Y') }}
         </h6>
+        <h6 class="mdl-typography--text-center">
+            Designed and Developed by <a href="http://amrittwanabasu.com.np" class="text--color-white text--decoration-none">Amrit Twanabasu</a>
+        </h6>
     </div>
     <div class="mdl-mega-footer__bottom-section">
         <!--Social Buttons-->
