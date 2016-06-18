a1a7affa922e105931365ad85022d30b9307e52b
Amrit Twanabasu <amrit.cas@gmail.com>
2016-06-18 13:22:16 +0545

    Make recent posts classes more generic

--- resources/views/_partials/posts/recent-posts.blade.php
+++ resources/views/_partials/posts/recent-posts.blade.php
@@ -1,26 +1,26 @@
-<div class="recent-posts mdl-card mdl-shadow--4dp">
+<div class="right-section-card mdl-card mdl-shadow--4dp">
     <div class="mdl-card__title">
         <h2 class="mdl-card__title-text">Recent Posts</h2>
     </div>
     <div class="mdl-card__supporting-text">
         @if(count($recentPosts) > 0)
-            <ul class="mdl-list recent-posts__list">
+            <ul class="mdl-list right-section-card__list">
                 @foreach($recentPosts as $post)
-                    <li class="recent-post mdl-list__item">
-                        <span class="mdl-grid mdl-list__item-primary-content recent-post__container">
-                            <div class="mdl-cell mdl-cell--3-col recent-post__thumbnail-container img--circular">
+                    <li class="right-section-list mdl-list__item">
+                        <span class="mdl-grid mdl-list__item-primary-content right-section-list__container">
+                            <div class="mdl-cell mdl-cell--3-col right-section-list__thumbnail-container img--circular">
                                 <img src="{{ asset('img/'.strtolower($post->category->category_name).'.png') }}"
                                      alt="{{ $post->post_title }}"
-                                     class="recent-post__thumbnail img--circular">
+                                     class="right-section-list__thumbnail img--circular">
                             </div>
-                            <div class="mdl-cell mdl-cell--9-col mdl-cell--3-col-phone mdl-cell--6-col-tablet recent-post__info">
-                                <h6 class="recent-post__title">
+                            <div class="mdl-cell mdl-cell--9-col mdl-cell--3-col-phone mdl-cell--6-col-tablet right-section-list__info">
+                                <h6 class="right-section-list__title">
                                     <a href="{{ route('posts.show',['posts' => $post->post_slug]) }}"
-                                       class="recent-post__title-link">
+                                       class="right-section-list__title-link">
                                         {{ $post->post_title_truncated }}
                                     </a>
                                 </h6>
-                                <div class="recent-post__detail">
+                                <div class="right-section-list__detail">
                                     {{ $post->published_at->diffForHumans() }} | {{ $post->category->category_name }}
                                     @if($post->isNotBlog())
                                         | {{ $post->grade_subject }}
