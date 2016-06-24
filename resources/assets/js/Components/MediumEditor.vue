ccee22dbd2896bfa22729fc484dda69cb577e077
Amrit Twanabasu <amrit.cas@gmail.com>
2016-06-24 22:27:05 +0545

    Add Various Extensions to MediumEditor and styled the editor

--- resources/assets/js/Components/MediumEditor.vue
+++ resources/assets/js/Components/MediumEditor.vue
@@ -1,13 +1,38 @@
 <template>
-    <textarea name="{{ name }}" id="{{ id }}" class="editor">{{ content }}</textarea>
+    <textarea name="{{ name }}" id="{{ id }}" class="editor post__body" placeholder="Write here...">{{ content }}</textarea>
 </template>
 
 <script>
     import MediumEditor from 'medium-editor';
+    import MediumEditorAutoList from './MediumEditor/AutoList';
 
     export default {
         ready() {
-            this.editor = new MediumEditor(this.$el);
+            this.editor = new MediumEditor(this.$el,{
+                toolbar: {
+                    buttons: [
+                        "bold",
+                        "italic",
+                        "anchor",
+                        "h2",
+                        "h3",
+                        "h4",
+                        "quote",
+                        "subscript",
+                        "superscript",
+                        "justifyLeft",
+                        "justifyCenter",
+                        "justifyRight",
+                        "justifyFull"
+                    ]
+                },
+                placeholder: {
+                    text: 'Write Here...'
+                },
+                extensions: {
+                    'autolist' : new MediumEditorAutoList()
+                }
+            });
 
             this.editor.subscribe('editableInput',function(event,editable){
                 this.content = editable.innerHTML;
