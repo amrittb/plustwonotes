acda63473c0f99d818bac0693cd14efecfcb9fd4
Amrit Twanabasu <amrit.cas@gmail.com>
2016-06-28 19:59:40 +0545

    Add Social pages buttons

--- resources/views/_partials/footer.blade.php
+++ resources/views/_partials/footer.blade.php
@@ -4,10 +4,21 @@
             Copyright &copy; Plus Two Notes<br/> {{ \Carbon\Carbon::now()->format('Y') }}
         </h6>
         <h6 class="mdl-typography--text-center">
-            Designed & Developed by <a href="http://amrittwanabasu.com.np" class="text--color-white text--decoration-none">Amrit Twanabasu</a>
+            Designed & Developed with &heartsuit; by <a href="http://amrittwanabasu.com.np" class="text--color-white text--decoration-none">Amrit Twanabasu</a>
         </h6>
     </div>
-    <div class="mdl-mega-footer__bottom-section">
-        <!--Social Buttons-->
+    <div class="mdl-mega-footer__bottom-section mdl-typography--text-center">
+        <h6>
+            Yes, of course we are on social sites. Who isn't these days?
+        </h6>
+        <a href="https://facebook.com/plustwonotes" class="text--decoration-none" target="_blank">
+            <img src="{{ asset('img/facebook-logo-button.png') }}" alt="Plus Two Notes's Facebook page">
+        </a>
+        <a href="https://plus.google.com/+Plustwonotespage" class="text--decoration-none" target="_blank">
+            <img src="{{ asset('img/google-plus-logo-button.png') }}" alt="Plus Two Notes's Google Plus page">
+        </a>
+        <a href="https://www.youtube.com/c/Plustwonotespage" class="text--decoration-none" target="_blank">
+            <img src="{{ asset('img/youtube-logo-button.png') }}" alt="Plus Two Notes's Youtube Channel page">
+        </a>
     </div>
 </footer>
