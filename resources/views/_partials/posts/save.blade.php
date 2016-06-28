5ab88f17507a48d03fd6e55f228ebe2e181a8961
Amrit Twanabasu <amrit.cas@gmail.com>
2016-06-28 19:32:14 +0545

    Add form hints to post save page

--- resources/views/_partials/posts/save.blade.php
+++ resources/views/_partials/posts/save.blade.php
@@ -1,59 +1,97 @@
 <!-- Post Title Input -->
-<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-    {!! Form::label('post_title','Post Title',['class'=>'mdl-textfield__label']) !!}
-    {!! Form::text('post_title',null,['class' => 'mdl-textfield__input']) !!}
+<div class="form-group--large">
+    <div class="form-group__info">
+        <i class="material-icons">info</i>
+        This post's title goes here.
+    </div>
+    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+        {!! Form::label('post_title','Post Title',['class'=>'mdl-textfield__label']) !!}
+        {!! Form::text('post_title',null,['class' => 'mdl-textfield__input']) !!}
+    </div>
 </div>
-
-<br />
 
 <!-- Post Slug Input -->
-<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-    {!! Form::label('post_slug','Post Slug',['class'=>'mdl-textfield__label']) !!}
-    {!! Form::text('post_slug',null,['class' => 'mdl-textfield__input']) !!}
+<div class="form-group--large">
+    <div class="form-group__info">
+        <i class="material-icons">info</i>
+        This post's slug goes here. Eg:
+        <span class="text--color-primary">
+            http://plustwonotes.com/posts/this-part-is-the-slug
+        </span>
+    </div>
+    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+        {!! Form::label('post_slug','Post Slug',['class'=>'mdl-textfield__label']) !!}
+        {!! Form::text('post_slug',null,['class' => 'mdl-textfield__input']) !!}
+    </div>
 </div>
 
-<br />
-
-<form-date-picker @if(isset($post)) date-time="{{ $post->published_at->format('M d, Y H:i') }}" @endif name="published_at" id="published_at" label="Published At"></form-date-picker>
-
-<br />
+<!-- Published At Input -->
+<div class="form-group--large">
+    <div class="form-group__info">
+        <i class="material-icons">info</i>
+        You can schedule this post for later or leave it as it is for default publish time.
+        It is Kathmandu's time (UTC+5:45)
+    </div>
+    <form-date-picker @if(isset($post)) date-time="{{ $post->published_at->format('M d, Y H:i') }}" @endif name="published_at" id="published_at" label="Published At"></form-date-picker>
+</div>
 
 <!-- Category Input -->
-<div class="mdl-textfield">
-    {!! Form::label('category_id','Category',['class' =>'mdl-textfield__label textfield__label--non-floating']) !!}
-    {!! Form::select('category_id',$categories) !!}
+<div class="form-group--large">
+    <div class="form-group__info">
+        <i class="material-icons">info</i>
+        Is this post a {{ join(" or ",array_values($categories)) }}? Select your category here.
+    </div>
+    <div class="mdl-textfield">
+        {!! Form::label('category_id','Category',['class' =>'mdl-textfield__label textfield__label--non-floating']) !!}
+        {!! Form::select('category_id',$categories) !!}
+    </div>
 </div>
-
-<br />
 
 <!-- Subject Input -->
-<div class="mdl-textfield">
-    {!! Form::label('subject_id','Subject',['class' =>'mdl-textfield__label textfield__label--non-floating']) !!}
-    {!! Form::select('subject_id',$subjects) !!}
+<div class="form-group--large">
+    <div class="form-group__info">
+        <i class="material-icons">info</i>
+        Does this post category needs an association with subject? Choose from this list. <br />
+        Note: Currently `Blog` category of post does not require post category. Other categories require a subject to be associated to.
+    </div>
+    <div class="mdl-textfield">
+        {!! Form::label('subject_id','Subject',['class' =>'mdl-textfield__label textfield__label--non-floating']) !!}
+        {!! Form::select('subject_id',$subjects) !!}
+    </div>
 </div>
 
-<br />
-
 <!-- Imp Input -->
-<label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="imp-checkbox">
-    {!! Form::checkbox('imp',0,null,['class' => 'mdl-checkbox__input','id' => 'imp-checkbox']) !!}
-    <span class="mdl-checkbox__label">Important?</span>
-</label>
-
-<br />
+<div class="form-group--large">
+    <div class="form-group__info">
+        <i class="material-icons">info</i>
+        Is this post important? Check this box and this post will be listed above in post lists.
+    </div>
+    <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="imp-checkbox">
+        {!! Form::checkbox('imp',0,null,['class' => 'mdl-checkbox__input','id' => 'imp-checkbox']) !!}
+        <span class="mdl-checkbox__label">Important?</span>
+    </label>
+</div>
 
 <!-- Featured Input -->
-<label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="featured-checkbox">
-    {!! Form::checkbox('featured',0,null,['class' => 'mdl-checkbox__input','id' => 'featured-checkbox']) !!}
-    <span class="mdl-checkbox__label">Featured?</span>
-</label>
-
-<br />
+<div class="form-group--large">
+    <div class="form-group__info">
+        <i class="material-icons">info</i>
+        Do you wish to feature this post? Check this box and this post will be shown in home page until other newer posts are featured.
+    </div>
+    <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="featured-checkbox">
+        {!! Form::checkbox('featured',0,null,['class' => 'mdl-checkbox__input','id' => 'featured-checkbox']) !!}
+        <span class="mdl-checkbox__label">Featured?</span>
+    </label>
+</div>
 
 <!-- Post Body Input -->
-<post-editor content="@if(isset($post)) {{ $post->post_body }} @endif"></post-editor>
-
-<br />
+<div class="form-group--large">
+    <div class="form-group__info">
+        <i class="material-icons">info</i>
+        This is the section where you edit a post. Write the post and save to draft. When the post is ready to be published check the tick mark from the post list and this post will be sent to verification. After verification, any publisher can publish the post.
+    </div>
+    <post-editor content="@if(isset($post)) {{ $post->post_body }} @endif"></post-editor>
+</div>
 
 <!-- Save Input -->
 {!! Form::button('Save',['type' => 'submit','class' => 'mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent']) !!}
