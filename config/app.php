047813a8b3c51a1cb30b835e39399dfcc39b7a1c
Amrit Twanabasu <amrit.cas@gmail.com>
2016-09-18 06:32:05 +0545

    Add intervention/image packge for Image Manipulation

--- config/app.php
+++ config/app.php
@@ -160,6 +160,7 @@ return [
 		 * Third-party Service Providers...
 		 */
         Collective\Html\HtmlServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class,
         Robbo\Presenter\PresenterServiceProvider::class,
 		Laracasts\Generators\GeneratorsServiceProvider::class,
 		Laracasts\Utilities\JavaScript\JavaScriptServiceProvider::class,
@@ -223,6 +224,7 @@ return [
 
         'Form'      => Collective\Html\FormFacade::class,
         'Html'      => Collective\Html\HtmlFacade::class,
-	],
+        'Image' => Intervention\Image\Facades\Image::class,
+    ],
 
 ];
