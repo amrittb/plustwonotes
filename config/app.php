24299fb7e54ced20cbc8b7007a364dc46e9bc5cb
Amrit Twanabasu <amrit.cas@gmail.com>
2016-09-25 08:32:46 +0545

    Add tymon/jwt-auth package for JWT Authentication

--- config/app.php
+++ config/app.php
@@ -164,6 +164,7 @@ return [
         Robbo\Presenter\PresenterServiceProvider::class,
 		Laracasts\Generators\GeneratorsServiceProvider::class,
 		Laracasts\Utilities\JavaScript\JavaScriptServiceProvider::class,
+		Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
 
 		/*
 		 * Application Providers...
@@ -224,7 +225,9 @@ return [
 
         'Form'      => Collective\Html\FormFacade::class,
         'Html'      => Collective\Html\HtmlFacade::class,
-        'Image' => Intervention\Image\Facades\Image::class,
+        'Image' 	=> Intervention\Image\Facades\Image::class,
+        'JWTAuth' 	=> Tymon\JWTAuth\Facades\JWTAuth::class,
+        'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
     ],
 
 ];
